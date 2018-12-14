e5089be147e4416ec5e8545978d74cf79ae9cc97
xenahort <juan.carlos.wow.95@gmail.com>
2018-12-14 18:03:59 +0100

    Lectura de datos completada

--- QAP genetic algorithm/src/qap/genetic/algorithm/main.java
+++ QAP genetic algorithm/src/qap/genetic/algorithm/main.java
@@ -5,17 +5,52 @@
  */
 package qap.genetic.algorithm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  *
  * @author juanca
  */
 public class main {
 
+    private static int n;
+    private static int flujos[][];
+    private static int distancias[][];
+
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Comienza...");
+        try (Scanner sc = new Scanner(new File("qap.datos/chr12c.dat"))) {
+            if (sc.hasNextInt()) {
+                n = sc.nextInt();
+                System.out.println("Tamaño del problema " + n);
+
+                flujos = new int[n][n];
+                distancias = new int[n][n];
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    flujos[i][j] = sc.nextInt();
+                    System.out.print(flujos[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    distancias[i][j] = sc.nextInt();
+                    System.out.print(distancias[i][j] + "\t");
+                }
+                System.out.println("");
+            }
+        }
     }
-    
+
 }
